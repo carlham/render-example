16f04b71df4c3b44d7a1b667b504a6533b26d447
carlham <carlkh@stud.ntnu.no>
2025-02-05 13:54:45 +0100

    Added API's from last lab

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing import Optional
-
+from pydantic import BaseModel
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -9,6 +9,54 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World, my name is Carl"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+
+class Fruit(BaseModel):
+    name: str
+    price: float
+    is_offer: bool | None = None
+
+FRUIT_DB = [
+    {'fruit_id': 0, 'name': 'Apple'},
+    {'fruit_id': 1, 'name': 'Orange'},
+    {'fruit_id': 2, 'name': 'Mango'}
+]
+
+@app.get('/items/{item_id}')
+def items(item_id: int, q: str | None = None):
+    for item in FRUIT_DB:
+        if item['fruit_id'] == item_id:
+            return item
+    return {'item': item_id, 'error': 'Item not found'}
+
+@app.get('/{path_str}')
+def read_string(path_str: str):
+    return {'you are on': path_str}
+
+@app.put('/items/{item_id}')
+def update_item(item_id: int, item: Fruit):
+    for fruit in FRUIT_DB:
+        if fruit['fruit_id'] == item_id:
+            fruit.update(item.dict())
+            return fruit
+    return {"error": "Item not found"}
+
+@app.post('/items')
+def create_item(item: Fruit):
+    new_fruit = item.dict()
+    new_fruit['fruit_id'] = len(FRUIT_DB)
+    FRUIT_DB.append(new_fruit)
+    return new_fruit
+
+@app.delete('/items/{item_id}')
+def delete_item(item_id: int):
+    for index, item in enumerate(FRUIT_DB):
+        if item['fruit_id'] == item_id:
+            del FRUIT_DB[index]
+            return {'fruit_id': item_id, 'len': len(FRUIT_DB)}
+    return {'fruit_id': item_id, 'error': 'Fruit not found'}
+
+
+
+if __name__ == '__main__':
+    import subprocess
+    subprocess.run(['uvicorn', 'my_api:app', '--reload'])
